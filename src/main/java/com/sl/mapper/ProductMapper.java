b8719116f77b0fce8ea20fef8487dfe4d302b5c7
SL <SL@DESKTOP-LIQIUD5>
2018-07-02 08:05:10 +0800

    搭建测试demo

---
+++ src/main/java/com/sl/mapper/ProductMapper.java
@@ -0,0 +1,9 @@
+package com.sl.mapper;
+import java.util.List;
+import com.sl.po.Product;
+
+public interface ProductMapper {
+	
+	List<Product> selectAllProduct();
+	
+}
---
+++ src/test/java/com/sl/test/TestClient.java
@@ -0,0 +1,59 @@
+package com.sl.test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.sl.po.Product;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+
+
+//使用mapper.userMapper.xml配置文件
+public class TestClient {
+
+	//定义SqlSession
+	SqlSession session =null;
+	
+	@Before
+	public void init() throws IOException {
+		//定义mabatis全局配置文件
+		String resource = "SqlMapConfig.xml";
+		
+		//加载mybatis全局配置文件
+		//InputStream inputStream = TestClient.class.getClassLoader().getResourceAsStream(resource);
+		
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+		SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+		SqlSessionFactory factory = builder.build(inputStream);
+		//根据sqlSessionFactory产生session
+		session = factory.openSession();	
+	}
+
+	
+	//查询所有user表所有数据
+	@Test
+	public void testSelectAllUser() {
+		String statement = "com.sl.mapper.ProductMapper.selectAllProduct";
+		List<Product> listProduct =session.selectList(statement);
+		for(Product product:listProduct)
+		{
+			System.out.println(product);
+		}
+		session.close();	
+	}
+	
+	
+}
