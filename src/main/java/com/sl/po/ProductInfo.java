00cae5278c4c678913fa86dc0883c736d5ffd6a2
shenlai <shlai244@163.com>
2018-07-03 18:42:45 +0800

    mapper配置文件 输入输出映射

---
+++ src/main/java/com/sl/po/ProductInfo.java
@@ -0,0 +1,50 @@
+package com.sl.po;
+
+import java.math.BigDecimal;
+
+public class ProductInfo {
+
+	private int Id;
+	private String ProductName;
+	private String Description;
+	private String CityCode;
+	private String CityName;
+	
+	
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public String getProductName() {
+		return ProductName;
+	}
+	public void setProductName(String productName) {
+		ProductName = productName;
+	}
+	public String getDescription() {
+		return Description;
+	}
+	public void setDescription(String description) {
+		Description = description;
+	}
+	public String getCityCode() {
+		return CityCode;
+	}
+	public void setCityCode(String cityCode) {
+		CityCode = cityCode;
+	}
+	public String getCityName() {
+		return CityName;
+	}
+	public void setCityName(String cityName) {
+		CityName = cityName;
+	}
+	
+	@Override
+    public String toString() {
+        return "Product [id=" + Id + ", ProductName=" + ProductName + ", Description=" + Description
+                + ", CityCode=" + CityCode + ", CityName=" + CityName + "]";
+    }
+}
--- src/test/java/com/sl/test/TestClient.java
+++ src/test/java/com/sl/test/TestClient.java
@@ -2,6 +2,7 @@ package com.sl.test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.math.BigDecimal;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -14,47 +15,126 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.sl.po.Product;
+import com.sl.po.ProductInfo;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
-
 //使用productMapper.xml配置文件
 public class TestClient {
 
-	//定义会话SqlSession
-	SqlSession session =null;
-	
+	// 定义会话SqlSession
+	SqlSession session = null;
+
 	@Before
 	public void init() throws IOException {
-		//定义mabatis全局配置文件
+		// 定义mabatis全局配置文件
 		String resource = "SqlMapConfig.xml";
-		
-		//加载mybatis全局配置文件
-		//InputStream inputStream = TestClient.class.getClassLoader().getResourceAsStream(resource);
-		
+
+		// 加载mybatis全局配置文件
+		// InputStream inputStream =
+		// TestClient.class.getClassLoader().getResourceAsStream(resource);
+
 		InputStream inputStream = Resources.getResourceAsStream(resource);
 		SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
 		SqlSessionFactory factory = builder.build(inputStream);
-		//根据sqlSessionFactory产生会话sqlsession
-		session = factory.openSession();	
+		// 根据sqlSessionFactory产生会话sqlsession
+		session = factory.openSession();
 	}
 
-	
-	//查询所有user表所有数据
-	@Test
-	public void testSelectAllUser() {
-		String statement = "com.sl.mapper.ProductMapper.selectAllProduct";
-		List<Product> listProduct =session.selectList(statement);
-		for(Product product:listProduct)
-		{
-			System.out.println(product);
-		}
-		//关闭会话
-		session.close();	
+	// select by id
+	// @Test
+	public void testSelectProduct() {
+		String statement = "com.sl.mapper.ProductMapper.selectProduct";
+		Product product = session.selectOne(statement, 1);
+		System.out.println(product);
+
+		session.close();
+	}
+
+	// insert
+	// @Test
+	public void testInsertProduct() {
+		String statement = "com.sl.mapper.ProductMapper.insertProduct";
+		Product product = new Product();
+		product.setName("小酒馆");
+		product.setDescription("xxxx");
+		product.setIsNew(true);
+		product.setUnitPrice(new BigDecimal(1000));
+		int count = session.insert(statement, product);
+		// System.out.println(product);
+		session.commit();// insert 需要commit
+		session.close();
+	}
+
+	// update
+	// @Test
+	public void testUpdateProduct() {
+		String statement = "com.sl.mapper.ProductMapper.updateProduct";
+		Product product = new Product();
+		product.setId(24);
+		product.setIsNew(true);
+		product.setUnitPrice(new BigDecimal(2000));
+
+		int count = session.update(statement, product);
+		// System.out.println(product);
+		session.commit();// insert 需要commit
+		session.close();
+	}
+
+	// update
+	// @Test
+	public void testProduct() {
+		String statement = "com.sl.mapper.ProductMapper.deleteProduct";
+
+		int count = session.delete(statement, 24);
+		// System.out.println(product);
+		session.commit();// insert 需要commit
+		session.close();
+	}
+
+	/*********************************************************************************************************************/
+	// 配置输入参数映射
+
+	// 见xml配置
+
+	// 配置输出结果集映射 resultType 和 resultMap
+	//@Test
+	public void testCountProduct() {
+		String statement = "com.sl.mapper.ProductMapper.countProducts";
+
+		int count = session.selectOne(statement);
+		System.out.println(count);
+		session.commit();// insert 需要commit
+		session.close();
 	}
 	
+	//resultMap
+	@Test
+	public void testselectProductInfo() {
+		String statement = "com.sl.mapper.ProductMapper.selectProductInfo";
+
+		List<ProductInfo> list = session.selectList(statement);
+		for (ProductInfo info : list) {
+			System.out.println(info);
+		}
+		session.close();
+	}
 	
+
+	/***********************************************************************************************************************/
+	// 查询所有user表所有数据
+	// @Test
+	public void testSelectAllUser() {
+		String statement = "com.sl.mapper.ProductMapper.selectAllProduct";
+		List<Product> listProduct = session.selectList(statement);
+		for (Product product : listProduct) {
+			System.out.println(product);
+		}
+		// 关闭会话
+		session.close();
+	}
+
 }
